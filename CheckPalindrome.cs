864170f863fc03113f0f8ecaaa6a3cdb84a741c7
25081997pc <chavanpooja2508@gmail.com>
2022-06-13 22:53:38 +0530

    Checking Prime Number is Palindrome or not

---
+++ CheckPalindrome.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeNumber
+{
+    public class CheckPalindrome
+    {
+        public void CheckPrimeIsPalindrome(int PrimeNumber)
+        {
+            int remainder, reverse = 0;
+            int pn = PrimeNumber;
+
+            while(pn > 0)
+            {
+                remainder = pn % 10;
+                pn = pn / 10;
+                reverse = reverse * 10 + remainder;
+            }
+            if(reverse == PrimeNumber)
+            {
+                Console.WriteLine("Number is Palindrome");
+            }
+            else
+            {
+                Console.WriteLine("Number is not Palindrome");
+            }
+        }
+        
+    }
+}
